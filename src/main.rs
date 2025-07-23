015380d914912a6182b25fd34c637fff836c8e19
Ayush Pramanik <ayushpramanik399@gmail.com>
2025-07-23 18:47:07 -0400

    update comments

--- src/main.rs
+++ src/main.rs
@@ -10,16 +10,15 @@ fn main() {
     app.run();
 
     // Get an output stream handle to the default physical sound device.
-    // Note that the playback stops when the stream_handle is dropped.
     let stream_handle =
         rodio::OutputStreamBuilder::open_default_stream().expect("open default audio stream");
 
-    // Load a sound from a file, using a path relative to Cargo.toml
-    let file = BufReader::new(File::open("test.mp3").unwrap());
-    // Note that the playback stops when the sink is dropped
-    let sink = rodio::play(&stream_handle.mixer(), file).unwrap();
+    // Load sound from file
+    let file_path = "test.mp3";
+    let file = BufReader::new(File::open(file_path).unwrap());
 
-    // The sound plays in a separate audio thread,
-    // so we need to keep the main thread alive while it's playing.
+    let _sink = rodio::play(&stream_handle.mixer(), file).unwrap();
+
+    // Since audio plays in seperate thread, block main thread from terminating
     std::thread::sleep(std::time::Duration::from_secs(5));
 }
