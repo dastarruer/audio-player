95ccd7cf88a01e6aacc090fd4ffd340dcd9a3649
Ayush Pramanik <ayushpramanik399@gmail.com>
2025-07-24 11:05:06 -0400

    Play audio in seperate thread while app runs

--- src/main.rs
+++ src/main.rs
@@ -4,21 +4,28 @@ use audio_player::App;
 use rodio::{Decoder, OutputStream, source::Source};
 use std::fs::File;
 use std::io::BufReader;
+use std::thread;
 
 fn main() {
+    let file_path = "test.mp3";
+    play_audio(file_path);
+
     let mut app = App::new();
     app.run();
+}
 
-    // Get an output stream handle to the default physical sound device.
-    let stream_handle =
-        rodio::OutputStreamBuilder::open_default_stream().expect("open default audio stream");
+fn play_audio(file_path: &'static str) {
+    thread::spawn(move || {
+        // Get an output stream handle to the default physical sound device.
+        let stream_handle =
+            rodio::OutputStreamBuilder::open_default_stream().expect("open default audio stream");
 
-    // Load sound from file
-    let file_path = "test.mp3";
-    let file = BufReader::new(File::open(file_path).unwrap());
+        // Load sound from file
+        let file = BufReader::new(File::open(file_path).unwrap());
 
-    let _sink = rodio::play(&stream_handle.mixer(), file).unwrap();
+        let _sink = rodio::play(&stream_handle.mixer(), file).unwrap();
 
-    // Since audio plays in seperate thread, block main thread from terminating
-    std::thread::sleep(std::time::Duration::from_secs(5));
+        // Since audio plays in seperate thread, block current thread from terminating
+        std::thread::sleep(std::time::Duration::from_secs(5));
+    });
 }
