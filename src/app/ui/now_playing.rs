47f0e676e94074a8644a4eca8060aac0974974ea
/home/dastarruer/.config/sops-nix/secrets/name </home/dastarruer/.config/sops-nix/secrets/email>
2025-08-12 12:29:27 -0400

    Return primary tag instead,and update test

--- src/app/ui/now_playing.rs
+++ src/app/ui/now_playing.rs
@@ -1,7 +1,7 @@
 use lofty::error::{ErrorKind, LoftyError};
 use lofty::file::TaggedFileExt;
 use lofty::read_from_path;
-use lofty::tag::Accessor;
+use lofty::tag::Tag;
 
 struct NowPlaying {}
 
@@ -10,23 +10,21 @@ impl NowPlaying {
         NowPlaying {}
     }
 
-    /// Parse an audio file's metadata, and return the first tag. This will contain details about the audio, such as the title, artist, etc.
+    /// Parse an audio file's metadata, and return the primary tag. This will contain details about the audio, such as the title, artist, etc.
     /// # Errors
     /// - If `path` does not exist
     /// - If the reader contains invalid data
-    /// - If the audio file does not contain a first tag
-    pub fn parse_file(path: &str) -> Result<(), LoftyError> {
+    /// - If the audio file does not contain a primary tag
+    pub fn parse_file(path: &str) -> Result<&Tag, LoftyError> {
         let tagged_file = read_from_path(path)?;
 
         // If the primary tag doesn't exist, or the tag types
-        // don't matter, the first tag can be retrieved
-        let unknown_first_tag = tagged_file
-            .first_tag()
+        // don't matter, the primary tag can be retrieved
+        let primary_tag = tagged_file
+            .primary_tag()
             .ok_or_else(|| LoftyError::new(ErrorKind::FakeTag))?;
 
-        println!("unknown first tag: {}", unknown_first_tag.title().unwrap());
-
-        Ok(())
+        Ok(primary_tag)
     }
 }
 
@@ -35,12 +33,15 @@ mod test {
     use super::*;
 
     mod parse_file {
+        use lofty::tag::Accessor;
+
         use super::*;
 
         #[test]
         fn parse_file() {
-            NowPlaying::parse_file("./test.mp3");
-            assert_eq!(1, 1)
+            let primary_tag = NowPlaying::parse_file("./test.mp3").unwrap();
+
+            assert_eq!(primary_tag.title().unwrap(), "less than lovers")
         }
     }
 }
