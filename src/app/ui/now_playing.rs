57ae561b949ffab11ccce451d348790f7731cb38
/home/dastarruer/.config/sops-nix/secrets/name </home/dastarruer/.config/sops-nix/secrets/email>
2025-08-19 17:05:23 -0400

    Add text_width param to text_center_x_of_widget
    This reduces the amount of times we have to get the text_width,
    and the number of parameters in the function

--- src/app/ui/now_playing.rs
+++ src/app/ui/now_playing.rs
@@ -31,12 +31,12 @@ impl NowPlaying {
 
         let title = NowPlaying::extract_title_from_tag(metadata_tag);
 
-        // TODO: Remove getting text width twice
+        // Get the width of the text
         draw::set_font(FONT, FONTSIZE);
         let (text_width, _) = draw::measure(&title, false);
 
         // Center X position
-        let center_x = NowPlaying::text_center_x_of_widget(cover_widget, &title, FONT, FONTSIZE);
+        let center_x = NowPlaying::text_center_x_of_widget(cover_widget, text_width);
         let pos_y = NowPlaying::get_title_widget_y(cover_widget);
 
         let title_widget_width = text_width + 10;
@@ -58,12 +58,12 @@ impl NowPlaying {
 
         let artist = NowPlaying::extract_artist_from_tag(metadata_tag);
 
-        // TODO: Remove getting text width twice
+        // Get the width of the text
         draw::set_font(FONT, FONTSIZE);
         let (text_width, _) = draw::measure(&artist, false);
 
         let artist_widget_x =
-            NowPlaying::text_center_x_of_widget(cover_widget, &artist, FONT, FONTSIZE);
+            NowPlaying::text_center_x_of_widget(cover_widget, text_width);
         let artist_widget_y = NowPlaying::get_artist_widget_y(title_widget);
 
         let artist_widget_width = text_width + 10;
@@ -122,10 +122,7 @@ impl NowPlaying {
     }
 
     /// Return the x position of a text box that would be needed to center it over another widget.
-    fn text_center_x_of_widget(widget: &Frame, title: &str, font: Font, fontsize: i32) -> i32 {
-        draw::set_font(font, fontsize);
-        let (text_width, _) = draw::measure(title, false);
-
+    fn text_center_x_of_widget(widget: &Frame, text_width: i32) -> i32 {
         // Get cover widget position and width
         let cover_x = widget.x();
         let cover_w = widget.w();
