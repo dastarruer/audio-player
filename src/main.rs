2c6d992d2720cbb9fb1eaca8b5f73c3c03d03c6d
/home/dastarruer/.config/sops-nix/secrets/name </home/dastarruer/.config/sops-nix/secrets/email>
2025-08-26 23:02:15 -0400

    Add debug for flexboxes
    Finally I can see the flexboxes im never using fltk again

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
 mod app;
 use app::AudioApp;
+use fltk_flex::Flex;
 fn main() {
+    // TODO: Remove this
+    // This colors the flexbox so it is easier to know what is going on
+    Flex::debug(true);
+
     let mut app = AudioApp::new();
     app.run();
 }
